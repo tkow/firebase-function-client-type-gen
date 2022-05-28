d5d9c059e32178e997092f94eab34ac1f172c597
tkow <t.kusama0613@outlook.jp>
2022-05-29 02:31:04 +0900

    feat: snapshot test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,32 +1,36 @@
+/* eslint-disable no-undef */
 import proxyquire from 'proxyquire';
 import path from 'path';
 import glob from 'glob';
 import { EOL } from 'os';
+import { expect } from 'chai';
+import { writeFileSync, readFileSync } from 'fs';
 import { outDefinitions } from '..';
 import { MOCKS } from './mock';
 
 const defaultFunctions = proxyquire('../fixtures/default', MOCKS);
+const snapshotsDir = path.resolve(__dirname, '../', 'snapshots')
 
 // Get document, or throw exception on error
 const sources = glob.sync(path.resolve(__dirname, '../', 'fixtures/default/**/*.ts'));
+const namedFunctions = proxyquire('../fixtures/named', MOCKS);
 
-try {
+it('named export function', () => {
   const result = outDefinitions(sources, defaultFunctions, {
     symbolConfig: {
       args: 'Params',
       result: 'Return',
     },
   });
-  console.log(result);
-  console.log(`default functions type generated${EOL}`);
-} catch (e) {
-  console.error(e);
-}
+  const snapPath = path.resolve(snapshotsDir, 'normal.snap.txt')
+  if (!process.env.SNAP) {
+    const expected = readFileSync(snapPath).toString();
+    expect(result).to.equal(expected)
+  }
+  writeFileSync(snapPath, result);
+})
 
-const namedFunctions = proxyquire('../fixtures/named', MOCKS);
-
-// Get document, or throw exception on error
-try {
+it('named export function', () => {
   const namedSources = sources.concat(glob.sync(path.resolve(__dirname, '../', 'fixtures/named/**/*.ts')));
   const result = outDefinitions(namedSources, namedFunctions, {
     symbolConfig: {
@@ -34,8 +38,10 @@ try {
       result: 'Return',
     },
   });
-  console.log(result);
-  console.log(`named functions type generated${EOL}`);
-} catch (e) {
-  console.error(e);
-}
+  const snapPath = path.resolve(snapshotsDir, 'nested.snap.txt')
+  if (!process.env.SNAP) {
+    const expected = readFileSync(snapPath).toString();
+    expect(result).to.equal(expected)
+  }
+  writeFileSync(snapPath, result);
+})
